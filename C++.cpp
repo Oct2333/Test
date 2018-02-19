bc25b68a783e020216c0e844c700d78ccea9edbd
Oct2333 <Oct2333@outlook.com>
2018-02-20 07:43:50 +0800

    客户端上传测试2

---
+++ C++.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+//using std::cout;  //使用std空间中cout对象。::调用的意思
+//using std::endl;
+int main(void)
+{
+    //cout<<"hello world"<<endl;
+    std::cout<<"hello world"<<std::endl;
+    return 0;
+}
